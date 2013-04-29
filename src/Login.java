55ebc2b573c8effe00e159954559445a3f437f57
Dean <deanburgos35@gmail.com>
2013-04-29 12:54:52 -0400

    renamed Login file, added Dan comments
    old file missing Daniel comments at top, used Login from Dan's latest
    email

---
+++ src/Login.java
@@ -0,0 +1,84 @@
+//Daniel Weston
+//Software Engineering II
+//Spring 2013
+//Assignment 6
+//My assigned task (i.e., module) is to program a screen to enter a username and password and, if that information is correct, then a screen is displayed containing the additional modules to be programmed by the other members of the group.
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.Toolkit;
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Login
+{
+
+	private static void createAndShowUI()		//create user interface to enter information
+	{
+		LoginPanel login = new LoginPanel();
+		int response = JOptionPane.showConfirmDialog(null, login, "Please Enter User Name and Password",
+		JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		if (response == JOptionPane.OK_OPTION) 
+		{
+			String name = login.getName();
+			String pWord = login.getPassword();
+
+			if (name.equals("Admin") && pWord.equals("Password"))   //If username and password correct, create next module
+			{
+				Window frame = new Window();
+				frame.setVisible(true);
+			}
+		
+			else	//if username and password is incorrect
+			{
+				String msg = "Incorrect name and password. \n(try Admin and Password)";
+				JOptionPane.showMessageDialog(null, msg);
+			}
+		}
+	}
+
+	public static void main(String[] args) 	//main
+	{
+		java.awt.EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				createAndShowUI();
+			}
+		});
+	}
+}
+
+
+class LoginPanel extends JPanel //code for login panel
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField nameField = new JTextField(10);
+	private JPasswordField passwordField = new JPasswordField(10);
+
+	public LoginPanel() 
+	{
+		setLayout(new GridLayout(2, 2, 5, 5)); 
+		add(new JLabel("Enter User Name:"));
+		add(nameField);
+		add(new JLabel("Enter Password:"));
+		add(passwordField);
+	}
+
+	public String getName() 	//method for user name
+	{
+		return nameField.getText();
+	}
+
+	public String getPassword() //method for password
+	{
+		return new String(passwordField.getPassword()); 
+	}
+
+}
